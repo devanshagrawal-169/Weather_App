fd7d0153efb8216c01f84d5a40e0125213099912
devanshagrawal-169 <83854973+devanshagrawal-169@users.noreply.github.com>
2023-06-24 14:58:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,123 @@
+const api = {
+    key: "2fa73590fd8b5a4c6e68098ad5625395",
+    base: "https://api.openweathermap.org/data/2.5/"
+};
+
+const searchbox = document.querySelector(".search-box");
+searchbox.addEventListener("keypress", setQuery);
+
+function setQuery(evt) {
+    if (evt.keyCode == 13) {
+        getResults(searchbox.value);
+    }
+}
+
+function getResults(query) {
+    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((weather) => {
+            return weather.json();
+        })
+        .then(displayResults);
+}
+
+function displayResults(weather) {
+    console.log(weather);
+    let city = document.querySelector(".location .city");
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    let now = new Date();
+    let date = document.querySelector(".location .date");
+    date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector(".current .temp");
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°C</span>`;
+
+    let weather_el = document.querySelector(" .weather");
+    weather_el.innerText = weather.weather[0].main;
+
+    let hilow = document.querySelector(".hi-low");
+    hilow.innerText = `${weather.main.temp}°C / ${weather.main.temp_max}°C`;
+
+
+    let fl = document.querySelector("#fl");
+    fl.innerHTML = `Feels like: ` + `${weather.main.feels_like}°C`;
+
+    let p = document.querySelector("#p");
+    p.innerHTML = `Pressure:
+  ` + `${weather.main.pressure} Pa`;
+
+    let hd = document.querySelector("#hd");
+    hd.innerHTML = `Humidity: ` + `${weather.main.humidity} g/Kg`;
+
+    let w = document.querySelector("#w");
+    w.innerHTML = `Wind Speed: ` + `${weather.wind.speed} Km/h`;
+
+
+}
+
+
+function dateBuilder(d) {
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+}
+
+function dateBuilder(d) {
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    ];
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return ` ${date}/ ${month}/ ${year} , ${day}`;
+}
